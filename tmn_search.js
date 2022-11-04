1d291e72f6c24ba1c15c596e53dcf0f0fe395a38
nicholasburka <nburka@mac.com>
2022-11-04 15:34:59 -0400

    commenting out broken line

--- tmn_search.js
+++ tmn_search.js
@@ -451,7 +451,7 @@ TRACKMENOT.TMNInjected = function() {
             //     window.setTimeout(clickButton, nextPress);
             // else
             //     window.setTimeout(pressEnter, nextPress, searchBox);
-            window.setTimeout( sendCurrentURL, nextpress+1) //no function called sendCurrentURL Q: ??
+            //window.setTimeout( sendCurrentURL, nextpress+1) //no function called sendCurrentURL
         }
     }
 
