3ac5bca6d4939176118f1a64840f2c8e1d4ac91f
mac <mac@macs-mbp.mynetworksettings.com>
2024-01-29 23:55:27 -0500

    Documented code - JSDoc

--- menu-script.js
+++ menu-script.js
@@ -7,37 +7,59 @@ if (chrome == undefined) {
 
 if(!TRACKMENOT) var TRACKMENOT = {};
 
+/** The TrackMeNot menu displayed from its browser icon.
+ * @exports TRACKMENOT.Menus
+ * @property {object} options - the current TMN options
+ * */
 TRACKMENOT.Menus = function() {
   var options = null;
   
 
 
     
-  
+  /** Logs an input msg to the console 
+   * @funciton _cout
+   * @inner
+   * @param {string} msg - the message to log
+   * */
   function  _cout (msg) { console.log(msg);  }
   
 
 
   return { 
-	  
+	  /** Opens a link to the TMN FAQ page.
+       * @function showHelp
+       * @inner
+       * */
    showHelp: function() {
     window.open("http://www.cs.nyu.edu/trackmenot/faq.html")
   },
-  
+  /** Toggles whether TMN is enabled or disabled in options, saves new options to local storage,
+    * and reloads the menu HTML.
+    * @function toggleOnOff
+    * @inner
+    * */
    toggleOnOff: function() {   
 	options.enabled = !options.enabled      
 
          api.storage.local.set({"options_tmn":options});
           TRACKMENOT.Menus.onLoadMenu({"options_tmn":options});
    },
-      
+  /** Changes the state of the useTab option, sets a new set of options in local storage, amnd reloads the HTML for the menu 
+    * @function toggleTabFrame
+    * @inner
+    * */
    toggleTabFrame: function() {
         options.useTab = !options.useTab
         api.storage.local.set({"options_tmn":options});
         TRACKMENOT.Menus.onLoadMenu({"options_tmn":options});  
       },
       
-
+      /** Loads the UI HTML for the menu, based on the values of the options within the input items.
+       * @function onLoadMenu
+       * @inner
+       * @param {object} items - container object for options_tmn to load into the menu
+       * */
      onLoadMenu: function( items ) {
          options = items["options_tmn"];
       
